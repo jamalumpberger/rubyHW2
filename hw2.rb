eca356b9cb5957e5431d2493eacf81dd7f71dc88
jamalumpberger <nozaki.kn@gmail.com>
2014-08-05 13:13:03 -0400

    first commit

---
+++ hw2.rb
@@ -0,0 +1,24 @@
+# # Create a program to analyze a large block of text 
+# and report back on the frequency of each word in the text.
+
+# # Start by sorting the text into a hash where each word is 
+# a key with the word’s value being the amount of times it 
+# has been used in the block:
+# # {the: 10, john: 1, of: 15}
+
+# # Once you’ve created this hash, 
+# return the word that has been used the most
+
+# # When you’re done, encapsulate your script 
+# inside of a method that can analyze any block of text fed to it
+
+puts "Insert block of text here: "
+text_block = gets.chomp
+
+separate_words = text_block.split(" ")
+hash_count = Hash.new(0)
+separate_words.each { |word| hash_count[word] += 1 }
+hash_count = hash_count.sort_by {|x, y|}
+hash_count.reverse!
+puts hash_count[0]
+# hash_count.each { |word, count| puts word + " " + .to_s }
